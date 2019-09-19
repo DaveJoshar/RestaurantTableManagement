7183e2cfef1814719eb84839c267cd16d348713c
DaveJoshar <joshidibya77@gmail.com>
2019-09-19 14:41:38 +0545

    First ever commit of .net project

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        td{
+            margin:10px;
+            padding:5px;
+        }
+    </style>
+    <div class="container">
+        <form>
+            <fieldset>
+                <legend>Reserve Table</legend>
+                <div class="col-md-6">
+                    <div class="col-md-6">
+                        Name:
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" data-bind="value:userName"/>
+                    </div>
+                    <div class="col-md-6">
+                        Contact Number:
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" data-bind="value:userContact" maxlength=10/>
+                    </div>
+                    <div class="col-md-6">
+                        Available Table:
+                    </div>
+                    <div class="col-md-6">
+                        <span data-bind="if:$root.availableTables().length!=0">
+                            <select data-bind="options:$root.availableTables, optionsText:'tableType', value:selectedTableId, optionsValue:$data,optionsCaption:'Choose..'"></select>
+                        </span>
+                        <span data-bind="ifnot: $root.availableTables().length!=0" style="color:red; margin-top:5px">
+                            No Table Available
+                        </span>
+                    </div>
+                    <div class="col-md-4"><button class="btn btn-primary" data-bind="click:addUser">Reserve</button></div>
+                </div>
+            </fieldset>   
+        </form>
+        <div style="margin:15px; padding:10px">
+            <table class="table table-striped">
+                <thead>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Contact Number</th>
+                    <th>Table Type</th>
+                </thead>
+                <tbody data-bind="foreach:users">
+                    <td>
+                        <span data-bind="text:userId"></span>
+                    </td>
+                    <td>
+                        <span data-bind="text:userName"></span>
+                    </td>
+                    <td>
+                        <span data-bind="text:userContact"></span>
+                    </td>
+                    <td>
+                        <span data-bind="text: selectedTableId().tableType"></span>
+                    </td>
+                    <td>
+                        <button data-bind="click: $root.cancelReservation" class="btn btn-danger">Cancel</button>
+                    </td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="practice1.js"></script>
+</asp:Content>
---
+++ WebApplication3/practice1.js
@@ -0,0 +1,100 @@
+﻿function __User(data) {
+    var self = this;
+    self.userId = ko.observable(data.userId);
+    self.userName = ko.observable(data.userName);
+    self.userContact = ko.observable(data.userContact);
+    self.selectedTableId = ko.observable(data.selectedTableId());
+}
+
+var __FoodOrderModel = function () {
+    var self = this;
+    self.userId = 1;
+    self.userName = ko.observable();
+    self.userContact = ko.observable();
+    self.users = ko.observableArray([]);
+    self.selectedTableId = ko.observable();
+    self.tables = ko.observableArray([
+        { tableId: 1, tableType: 'Family Dining Table', status:1 },
+        { tableId: 2, tableType: '2 Person Table', status:1},
+        { tableId: 3, tableType: '4 Person Table', status:1 },
+        { tableId: 4, tableType: 'OutDoor Table',status:1 },
+    ]);
+    //to bind available table only 
+    self.availableTables = ko.observableArray([]);
+
+    //to show only available tables
+    self.updateAvailableTable = function () {
+        
+        for (i = 0; i < self.tables().length; i++) {
+            if (self.tables()[i].status == 1) {
+                self.availableTables.push(self.tables()[i]);
+            }
+        }
+    }
+    self.findTable = function (data) {
+        return((self.tables().find(x => x.tableId == data().tableId)));
+    }
+    self.updateAvailableTable();
+    //to add new user
+    self.addUser = function () {
+        if (self.verify()) {
+            if (confirm("Are you sure?")) {
+                var tempArray = {
+                    userId: self.userId,
+                    userName: self.userName(),
+                    userContact: self.userContact(),
+                    selectedTableId: self.selectedTableId
+                };
+
+                self.userId += 1;
+                self.users.push(new __User(tempArray));
+                var sucessMsg = "Table reserved";
+                //(self.tables().find(x => x.tableId == self.selectedTableId().tableId)).status = 0;
+                var t = self.findTable(self.selectedTableId);
+                t.status = 0;
+                self.contentClear();
+                self.updateAvailableTable();
+                alert(sucessMsg);
+            }
+        }
+    };
+    //to clear content
+    self.contentClear = function () {
+        self.userName(null);
+        self.userContact(null);
+        self.selectedTableId(null);
+        self.availableTables.removeAll();
+    };
+    //to verify null
+    self.verify = function () {
+        if (self.userName() == null ||
+            self.userContact() == null ||
+            self.selectedTableId() == null ||
+            self.userName() == undefined ||
+            self.userContact() == undefined ||
+            self.selectedTableId() == undefined)
+        {
+            alert("Enter data first");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //to cancel reservation
+    self.cancelReservation = function (data) {
+        if (confirm("Are you sure?")) {
+            self.users.remove(data);
+            //(self.tables().find(x => x.tableId == data.selectedTableId().tableId)).status = 1;
+            self.contentClear();
+            
+            var t = self.findTable(data.selectedTableId);
+            t.status = 1;
+            self.updateAvailableTable();
+            alert('Table Canceled');
+
+        }
+    }
+}
+
+ko.applyBindings(new __FoodOrderModel());
